eb138b4192b2287c721c39225dcc720b958e00d1
surendra-chouhan <chouhansurendra028@gmail.com>
2021-04-19 21:46:17 +0530

    Added logic to calculate total amount won or lost after 20 days

--- gamblingSimulator.js
+++ gamblingSimulator.js
@@ -4,15 +4,23 @@ var stake = 100;
 var bet = 1;
 const WON = 1;
 const LOST = 0;
+var won_total = 0;
+var lost_total = 0;
 
-
-while(stake > 50 && stake < 150){
-    var random = Math.floor(Math.random() * 2);
-    if(random == WON){
-        stake++;
+for(let days = 1; days <= 20; days++){
+    while(stake > 50 && stake < 150){
+        var random = Math.floor(Math.random() * 2);
+        if(random == WON){
+            stake++;
+            won_total += stake;
+        }
+        else{
+            stake--;
+            lost_total += stake;
+        }
+        console.log(stake);
     }
-    else{
-        stake--;
-    }
-    console.log(stake);
 }
+
+console.log("Total Won : ", won_total);
+console.log("Total Lost : ", lost_total);
